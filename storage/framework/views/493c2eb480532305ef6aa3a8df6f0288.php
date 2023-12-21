d4ac0daf451a6618546d43efa0afb246abe0a6f8
nadya putri dwinta <142197856+Nadyaputridwinta16@users.noreply.github.com>
2023-12-22 00:44:22 +0700

    Add files via upload

---
+++ storage/framework/views/493c2eb480532305ef6aa3a8df6f0288.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Portal-Mahasiswa 2023</span>
+        </div>
+    </div>
+</footer><?php /**PATH D:\Kuliah\Sakti Aswandi Sitanggang\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
